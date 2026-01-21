aa1098bda8e2d411c1d7e059226048ceb6639bff
xuzichun <2978500991@qq.com>
2026-01-21 17:02:39 +0800

    start

--- demo/src/com/x/hello/hello.java
+++ demo/src/com/x/hello/hello.java
@@ -1,4 +1,12 @@
 package com.x.hello;
 
 public class hello {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+
+    }
+ //抢红包线
+
+
 }
